3b12f5342f0828709b502b9f448429d6d911c11d
Apurva101 <apurvasingh2726@gmail.com>
2020-08-01 18:09:56 +0530

    added question component

--- qnapp/src/App.js
+++ qnapp/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import NavBar from './Components/NavBar.js';
 import Questions from './Questions/Questions.js';
+import Question from './Question/Question.js';
 
 function App() {
   return (
    <div>
    <NavBar/>
-   <Questions/>
+   <Route exact path='/' component={Questions}/>
+   <Route exact path='/question/:questionId' component={Question}/>
    </div>
   );
 }
